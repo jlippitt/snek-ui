b65b06bd1189567ff9b795f4de0c81f76dbb18a2
James Lippitt <james.lippitt@gmail.com>
2019-08-24 08:43:59 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ]
+  },
+  node: {
+    fs: 'empty',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+    modules: ['./src', './node_modules'],
+  },
+  output: {
+    filename: 'snek.js',
+    path: path.resolve(__dirname, 'dist'),
+  }
+};
